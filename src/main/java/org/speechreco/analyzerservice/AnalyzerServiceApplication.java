3abee0b2dbb15ffecaed24b0e6efd5e65132f8ca
Maksym Shyiko <63159095+M-Bell@users.noreply.github.com>
2024-06-07 16:29:46 +0200

    Update AnalyzerServiceApplication.java

--- src/main/java/org/speechreco/analyzerservice/AnalyzerServiceApplication.java
+++ src/main/java/org/speechreco/analyzerservice/AnalyzerServiceApplication.java
@@ -5,7 +5,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-//git token: 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
 //docker run -it --rm --name rabbitmq -p 5672:5672 -p 15672:15672 rabbitmq:3.13-management
 @SpringBootApplication()
 public class AnalyzerServiceApplication {
